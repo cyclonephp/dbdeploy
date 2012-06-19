a002a753e009f03dcb6bfb5c6778cde58cd6e37e
Bence Eros <crystal@cyclonephp.org>
2012-06-19 21:04:27 +0200

    created generic exception class for dbdeploy

---
+++ classes/cyclone/dbdeploy/Exception.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace cyclone\dbdeploy;
+
+/**
+ * Generic exception class for the DbDeploy tool.
+ *
+ * @author Bence Erős <crystal@cyclonephp.org>
+ * @package dbdeploy
+ */
+class Exception extends \Exception {
+
+}
