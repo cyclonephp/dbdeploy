abae73396906d18ba342b32250e17005af68feb8
Bence Eros <crystal@cyclonephp.org>
2012-06-19 21:03:53 +0200

    added default config values

--- config/dbdeploy.php
+++ config/dbdeploy.php
@@ -1,5 +1,10 @@
 <?php
 
 return array(
-	'changelog_table' => 'changelog'
+	'changelog_table' => 'changelog',
+    'delta_set' => '',
+    'connection' => 'default',
+    'src_dir' => 'schema',
+    'exec' => FALSE,
+    'quiet' => FALSE
 );
