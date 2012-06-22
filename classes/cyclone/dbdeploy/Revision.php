34eb6ccf88d2ccb4af57f3a415c4107aaf5a935f
Bence Eros <crystal@cyclonephp.org>
2012-06-22 20:17:04 +0200

    implemented & tested Revision class static accessor methods

--- classes/cyclone/dbdeploy/Revision.php
+++ classes/cyclone/dbdeploy/Revision.php
@@ -14,6 +14,51 @@ namespace cyclone\dbdeploy;
  */
 class Revision {
 
+    protected static $_storage = array();
+
+    /**
+     * Returns the revision instances which belong to the given delta set.
+     * The return value is an array of revision number =&gt; @c Revision instance
+     * pairs.
+     *
+     * @param $delta_set
+     * @return Revision
+     * @throws Exception if the delta set is not loaded.
+     */
+    public static function get_by_delta_set($delta_set) {
+        if ( ! isset(self::$_storage[$delta_set]))
+            throw new Exception("delta set '$delta_set' is not loaded'");
+
+        return self::$_storage[$delta_set];
+    }
+
+    /**
+     * Queries the revision according to the given delta set and revision number.
+     *
+     * @param $delta_set string the name of the delta set
+     * @param $revision_number
+     * @return Revision
+     * @throws Exception if the delta set is not loaded or the revision in the
+     *  delta set is not loaded.
+     * @uses get_by_delta_set()
+     */
+    public static function get_by($delta_set, $revision_number) {
+        $delta_set_revisions = self::get_by_delta_set($delta_set);
+        if ( ! isset($delta_set_revisions[$revision_number]))
+            throw new Exception("revision '$revision_number' in delta set '$delta_set' is not loaded");
+
+        return $delta_set_revisions[$revision_number];
+    }
+
+    /**
+     * Clears the internal @c Revision instance storage.
+     *
+     * Only for unittests.
+     */
+    public static function clear_storage() {
+        self::$_storage = array();
+    }
+
     /**
      * The "commit" part of the source file (before -- //UNDO)
      *
@@ -46,6 +91,15 @@ class Revision {
         $this->_delta_set = $delta_set;
         $this->_revision_number = $revision_number;
         list($this->_commit, $this->_undo) = $this->extract_commit_undo($src);
+        if ( ! isset(self::$_storage[$delta_set])) {
+            self::$_storage[$delta_set] = array();
+        }
+
+        if (isset(self::$_storage[$delta_set][$revision_number]))
+            throw new Exception("invalid state: cannot create two Revision instances "
+                 . "for revision #$revision_number in delta set '$delta_set'");
+
+        self::$_storage[$delta_set][$revision_number] = $this;
     }
 
     public function extract_commit_undo($src) {
--- tests/DBDeploy/DBDeploy_RevisionTest.php
+++ tests/DBDeploy/DBDeploy_RevisionTest.php
@@ -7,6 +7,11 @@ use cyclone\dbdeploy;
  */
 class DBDeploy_RevisionTest extends Kohana_Unittest_TestCase {
 
+    public function setUp() {
+        parent::setUp();
+        dbdeploy\Revision::clear_storage();
+    }
+
     public function test_constructor() {
         $rev = new dbdeploy\Revision('', 'ds', 10);
         $this->assertEquals('ds', $rev->delta_set);
@@ -29,4 +34,54 @@ undo
 -- //@UNDO', 'ds', 10);
     }
 
+
+    /**
+     * @expectedException \cyclone\dbdeploy\Exception
+     */
+    public function test_constructor_failure() {
+        $rev = new dbdeploy\Revision('commit
+        -- //@UNDO
+undo', 'ds', 10);
+        $this->assertEquals('commit', $rev->commit);
+        $this->assertEquals('undo', $rev->undo);
+
+        $rev = new dbdeploy\Revision('commit
+        -- //@UNDO
+undo', 'ds', 10);
+    }
+
+    public static function load_sample_revisions() {
+        new dbdeploy\Revision('commit1
+-- //@UNDO
+undo1', 'ds', 1);
+        new dbdeploy\Revision('commit2
+-- //@UNDO
+undo2', 'ds', 2);
+        new dbdeploy\Revision('commit3
+-- //@UNDO
+undo3', 'ds', 3);
+    }
+
+    /**
+     * @expectedException cyclone\dbdeploy\Exception
+     */
+    public function test_get_by_delta_set() {
+        $this->load_sample_revisions();
+        $revisions = dbdeploy\Revision::get_by_delta_set('ds');
+        $this->assertEquals(3, count($revisions), " 3 revisions loaded");
+        $this->assertEquals(array(1, 2, 3), array_keys($revisions), "revisions are indexed properly");
+
+        dbdeploy\Revision::get_by_delta_set('nonexistent');
+    }
+
+    /**
+     * @expectedException cyclone\dbdeploy\Exception
+     */
+    public function test_get_by() {
+        $this->load_sample_revisions();
+        $revision = dbdeploy\Revision::get_by('ds', 1);
+        $this->assertEquals(1, $revision->revision_number);
+
+        dbdeploy\Revision::get_by('ds', 20);
+    }
 }
